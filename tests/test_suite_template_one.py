c7cf03393767a59d6123f1647af320064dd4c48a
Oliver Glandberger <oliver.glandberger@qualisys.se>
2024-11-30 14:48:31 +0100

    Fixed issues with tests, added additional folders to CMakeLists.txt file-copying.

--- tests/test_suite_template_one.py
+++ tests/test_suite_template_one.py
@@ -1,5 +1,6 @@
 import unittest
-from text_parser import src_func
+
+from src.text_parser import src_func
 
 class TestSuiteTemplateOne(unittest.TestCase):
     def test_feature_a(self):
@@ -8,7 +9,7 @@ class TestSuiteTemplateOne(unittest.TestCase):
         Placeholder for a test that validates Feature A.
         Remove 'self.assertTrue(True)' and add real test logic.
         """
-        self.assertTrue(src_func() == 1)  # Auto-pass. Replace with actual test logic.
+        self.assertTrue(src_func() == 1)
 
 if __name__ == "__main__":
     unittest.main()
